b6753e682e1a0e5d71952d1b9a5f358f989525ea
Cody Garrett <cody.l.garrett22@gmail.com>
2019-09-26 08:19:30 -0600

    almost finished with details of page

--- main.js
+++ main.js
@@ -10,9 +10,49 @@ function toggleDisplay() {
 
 //hide team desc
 function hideModal() {
-    let targetClass = event.target.className;
+    let modal = document.getElementsByClassName("modal");
 
-    if(targetClass === 'modal') {
-       targetClass.classList.add("hidden");
-    }
+   document.getElementById("desc1").classList.add("hidden");
+}
+
+function hideModal2() {
+    let modal = document.getElementsByClassName("modal");
+
+   document.getElementById("desc2").classList.add("hidden");
+}
+
+function hideModal3() {
+    let modal = document.getElementsByClassName("modal");
+
+   document.getElementById("desc3").classList.add("hidden");
+}
+
+function hideModal4() {
+    let modal = document.getElementsByClassName("modal");
+
+   document.getElementById("desc4").classList.add("hidden");
+}
+
+function hideModal5() {
+    let modal = document.getElementsByClassName("modal");
+
+   document.getElementById("desc5").classList.add("hidden");
+}
+
+function hideModal6() {
+    let modal = document.getElementsByClassName("modal");
+
+   document.getElementById("desc6").classList.add("hidden");
+}
+
+function hideModal7() {
+    let modal = document.getElementsByClassName("modal");
+
+   document.getElementById("desc7").classList.add("hidden");
+}
+
+function hideModal8() {
+    let modal = document.getElementsByClassName("modal");
+
+   document.getElementById("desc8").classList.add("hidden");
 }
